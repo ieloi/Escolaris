eb97fce2ceeb5591cadf6ec19c8be1fd74a8c532
Ícaro Matheus <icaromatheuseloi@gmail.com>
2022-01-18 14:31:29 -0300

    Refatoração da Listagem de Escolas utilizando Formik

--- src/components/ListagemEscolas/index.js
+++ src/components/ListagemEscolas/index.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import { Card, Container, Row, Table, Col, Form, Button, Spinner } from "react-bootstrap";
 import axios from 'axios';
 import NotificationAlert from "react-notification-alert";
+import { useFormik } from 'formik';
 
 const ListagemEscolas = () => {
 
@@ -37,16 +38,10 @@ const ListagemEscolas = () => {
         { name: 'Distrito Federal', value: 'df' }
     ];
 
-    const termos_busca = {
-        nome: '',
-        estado: '',
-        municipio: ''
-    };
-
     const [listaMunicipios, setlistaMunicipios] = useState(null);
     const [resultadosEncontrados, setresultadosEncontrados] = useState(null);
-    const [termo, setTermo] = useState(termos_busca);
-    const [inputMunicipios, setinputMunicipios] = useState(true); //habilita ou desabilita o input
+    const [estadoSelecionado, setestadoSelecionado] = useState('');
+    const [esconderInputMunicipio, setesconderInputMunicipio] = useState(true); //habilita ou desabilita o input
     const notificationAlertRef = useRef(null); //usado pelo template para gerar notificações
     const [spinner, setspinner] = useState(false);
 
@@ -91,132 +86,151 @@ const ListagemEscolas = () => {
     }
 
     const handleChangeEstado = (e) => {
-        const { name, value } = e.target;
+        const { value } = e.target;
 
         if (value != "") {
-            setTermo({ ...termo, [name]: value });
-            const endopint_estado = `api/cidades/${value}`;
+            setestadoSelecionado(value);
+            let endopint_estado = `api/cidades/${value}`;
             buscarMunicipios(endopint_estado);
-            setinputMunicipios(false);
+            setesconderInputMunicipio(false);
         } else {
-            setTermo({ ...termo, estado: '', municipio: '' });
-            setinputMunicipios(true);
+            setestadoSelecionado(value);
+            setesconderInputMunicipio(true);
         }
 
     }
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setTermo({ ...termo, [name]: value });
-    }
-
-    const pesquisarEscolas = (e) => {
-        e.preventDefault();
+    const pesquisarEscolas = values => {
 
         let busca = ``;
 
-        if ((termo.nome.length < 3 || termo.nome == "") && termo.municipio == "") {
-            notify("tr", "danger", "Para a busca ser realizada é necessario preencher o nome (3 ou mais letras) ou escolher um municipio");
-        } else if (termo.nome.length >= 3 && termo.municipio != "") {
-            busca = `api/escolas/buscaavancada?cidade=${termo.municipio}&nome=${termo.nome}`;
+        if (values.nome.length >= 3 && values.municipio != "") {
+            busca = `api/escolas/buscaavancada?cidade=${values.municipio}&nome=${values.nome}`;
             buscarEscolasAPI(busca);
-            setTermo({ nome: '', estado: '', municipio: '' });
-            setinputMunicipios(true);
-        } else if (termo.nome.length >= 3 && termo.municipio == "") {
-            busca = `api/escolas?nome=${termo.nome}`;
+        } else if (values.nome.length >= 3 && values.municipio == "") {
+            busca = `api/escolas?nome=${values.nome}`;
             buscarEscolasAPI(busca);
-            setTermo({ nome: '', estado: '', municipio: '' });
-            setinputMunicipios(true);
-        } else if ((termo.nome.length < 3 || termo.nome == "") && termo.municipio != "") {
-            busca = `api/escolas/buscaavancada?cidade=${termo.municipio}`;
+        } else if ((values.nome.length < 3 || values.nome == "") && values.municipio != "") {
+            busca = `api/escolas/buscaavancada?cidade=${values.municipio}`;
             buscarEscolasAPI(busca);
-            setTermo({ nome: '', estado: '', municipio: '' });
-            setinputMunicipios(true);
         }
+
+        setestadoSelecionado('');
+        setesconderInputMunicipio(true);
     }
 
-    return (<>
-        <div className="rna-container">
-            <NotificationAlert ref={notificationAlertRef} />
-        </div>
-        <Container fluid>
-            <Row>
-                <Card className="w-100 p-3">
-                    <Card.Title as="h3" className="mb-3 mt-2">Pesquisar Escola</Card.Title>
-                    <Form onSubmit={pesquisarEscolas}>
-                        <Row>
-                            <Form.Group as={Col} md="6" className="mb-3">
-                                <Form.Label htmlFor="nomeEscola">Nome da Escola:</Form.Label>
-                                <Form.Control type="text" id="nomeEscola" name="nome" placeholder="Ex: Escola Lorem Ipsum" onChange={handleChange} value={termo.nome} />
-                            </Form.Group>
+    //Validação de dados com o Formik
+    const validate = values => {
+        const errors = {}
 
-                            <Form.Group as={Col} md="3" className="mb-3">
-                                <Form.Label htmlFor="selectEstado">Estado da Escola</Form.Label>
-                                <Form.Control id="selectEstado" name="estado" as="select" onChange={handleChangeEstado} value={termo.estado} >
-                                    <option value="">Selecione uma opção:</option>
-                                    {
-                                        listaEstados.map(estado => (
-                                            <option key={estado.value} value={estado.value}>{estado.name}</option>
-                                        ))
-                                    }
-                                </Form.Control>
-                            </Form.Group>
+        if ((values.nome.length < 3 || !values.nome) && !values.municipio) {
+            errors.ambos = "Para a busca ser realizada é necessario preencher o nome (3 ou mais letras) e/ou escolher um municipio"
+        }
 
-                            <Form.Group as={Col} md="3" className="mb-3" hidden={inputMunicipios}>
-                                <Form.Label htmlFor="selectMunicipio">municipio da Escola</Form.Label>
-                                <Form.Control id="selectMunicipio" name="municipio" as="select" onChange={handleChange} value={termo.municipio} >
-                                    <option value="">Selecione uma opção:</option>
-                                    {
-                                        listaMunicipios && listaMunicipios.map(municipio => (
-                                            <option key={municipio[0]} value={municipio[0]}>{municipio[1]}</option>
-                                        ))
-                                    }
-                                </Form.Control>
-                            </Form.Group>
-                        </Row>
-                        <Button
-                            className="btn-fill pull-right"
-                            type="submit"
-                            variant="primary"
-                        >
-                            Enviar Dados
-                        </Button>
-                    </Form>
-                </Card>
-            </Row>
+        return errors;
+    }
 
-            <Row>
-                <Card className="w-100">
-                    <Card.Body className="table-full-width table-responsive">
-                        <Card.Title as="h3" className="mb-3 mt-2">Resultados Encontrados</Card.Title>
-                        <Table className="table-hover" responsive="sm">
-                            <thead>
-                                <tr>
-                                    <th>Nome</th>
-                                    <th>Estado</th>
-                                    <th>Cidade</th>
-                                    <th>Dependencia Administrativa</th>
-                                    <th>Situacao de Funcionamento</th>
+    //Criação de Formulário com o Formik
+    const formik = useFormik({
+        initialValues: {
+            nome: '',
+            municipio: ''
+        },
+        validate,
+        onSubmit: (values, { resetForm }) => {
+            pesquisarEscolas(values);
+            resetForm();
+        }
+    });
+
+return (<>
+    <div className="rna-container">
+        <NotificationAlert ref={notificationAlertRef} />
+    </div>
+    <Container fluid>
+        <Row>
+            <Card className="w-100 p-3">
+                <Card.Title as="h3" className="mb-3 mt-2">Pesquisar Escola</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">A pesquisa pode ser realizada de 3 formas: Pelo nome, Pelo nome e municipio e somente pelo municipio</Card.Subtitle>
+                <Form onSubmit={formik.handleSubmit}>
+                    {formik.errors.ambos ? (
+                        <div className="text-muted text-danger">
+                            {formik.errors.ambos}
+                        </div>
+                    ) : null}
+                    <Row>
+                        <Form.Group as={Col} md="6" className="mb-3">
+                            <Form.Label htmlFor="nomeEscola">Nome da Escola:</Form.Label>
+                            <Form.Control type="text" id="nomeEscola" name="nome" placeholder="Ex: Escola Lorem Ipsum" onChange={formik.handleChange} value={formik.values.nome} />
+                        </Form.Group>
+
+                        <Form.Group as={Col} md="3" className="mb-3">
+                            <Form.Label htmlFor="selectEstado">Estado da Escola</Form.Label>
+                            <Form.Control id="selectEstado" name="estado" as="select" onChange={handleChangeEstado} value={estadoSelecionado} >
+                                <option value="">Selecione uma opção:</option>
+                                {
+                                    listaEstados.map(estado => (
+                                        <option key={estado.value} value={estado.value}>{estado.name}</option>
+                                    ))
+                                }
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group as={Col} md="3" className="mb-3" hidden={esconderInputMunicipio}>
+                            <Form.Label htmlFor="selectMunicipio">municipio da Escola</Form.Label>
+                            <Form.Control id="selectMunicipio" name="municipio" as="select" onChange={formik.handleChange} value={formik.values.municipio} >
+                                <option value="">Selecione uma opção:</option>
+                                {
+                                    listaMunicipios && listaMunicipios.map(municipio => (
+                                        <option key={municipio[0]} value={municipio[0]}>{municipio[1]}</option>
+                                    ))
+                                }
+                            </Form.Control>
+                        </Form.Group>
+                    </Row>
+                    <Button
+                        className="btn-fill pull-right"
+                        type="submit"
+                        variant="primary"
+                    >
+                        Enviar Dados
+                    </Button>
+                </Form>
+            </Card>
+        </Row>
+
+        <Row>
+            <Card className="w-100">
+                <Card.Body className="table-full-width table-responsive">
+                    <Card.Title as="h3" className="mb-3 mt-2">Resultados Encontrados</Card.Title>
+                    <Table className="table-hover" responsive="sm">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Estado</th>
+                                <th>Cidade</th>
+                                <th>Dependencia Administrativa</th>
+                                <th>Situacao de Funcionamento</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {resultadosEncontrados && resultadosEncontrados.map(escola => (
+                                <tr key={escola.cod}>
+                                    <td>{escola.nome}</td>
+                                    <td>{escola.estado}</td>
+                                    <td>{escola.cidade}</td>
+                                    <td>{escola.dependenciaAdministrativaTxt}</td>
+                                    <td>{escola.situacaoFuncionamentoTxt}</td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {resultadosEncontrados && resultadosEncontrados.map(escola => (
-                                    <tr key={escola.cod}>
-                                        <td>{escola.nome}</td>
-                                        <td>{escola.estado}</td>
-                                        <td>{escola.cidade}</td>
-                                        <td>{escola.dependenciaAdministrativaTxt}</td>
-                                        <td>{escola.situacaoFuncionamentoTxt}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </Table>
-                    </Card.Body>
-                </Card>
-            </Row>
-        </Container>
-        {spinner && <Spinner animation="grow" style={{ position: 'absolute', left: '45%', top: '50%', width: '40px', height: '40px' }} />}
-    </>)
+                            ))}
+                        </tbody>
+                    </Table>
+                </Card.Body>
+            </Card>
+        </Row>
+    </Container>
+    {spinner && <Spinner animation="grow" style={{ position: 'absolute', left: '45%', top: '50%', width: '40px', height: '40px' }} />}
+</>)
 }
 
 export default ListagemEscolas;
